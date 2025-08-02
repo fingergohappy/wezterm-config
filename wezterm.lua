238bddf086d982b983832a0663ea846cc0e53008
fingerfrings <fingergohappy@163.com>
2025-08-02 13:25:37 +0800

    init

--- wezterm.lua
+++ wezterm.lua
@@ -5,15 +5,29 @@ local mux = wezterm.mux
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Catppuccin Mocha'
-config.font = 
-	wezterm.font('MonacoLigaturized Nerd Font Mono', { weight = 'Bold', italic = true })
+-- config.color_scheme = 'Catppuccin Mocha'
+-- config.color_scheme = 'Catppuccin Latte'
+-- config.color_scheme = 'Tokyo Night Day'
+-- config.color_scheme = 'Tokyo Night Moon'
+-- config.color_scheme = 'Solarized (light) (terminal.sexy)'
+-- config.color_scheme = 'Monokai (light) (terminal.sexy)'
+-- config.color_scheme = 'Monokai Dark (Gogh)'
+-- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+-- config.color_scheme = 'Monokai Vivid'
+config.color_scheme = 'Monokai Pro (Gogh)'
+
+
+config.font =
+-- wezterm.font('MonacoLigaturized Nerd Font Mono', { weight = 'Bold', italic = true })
+    wezterm.font('Maple Mono NF', { weight = 'Bold', italic = true })
+-- MapleMono-NF-Regular
 config.bold_brightens_ansi_colors = "BrightOnly"
-config.font_size = 16
+config.font_size = 15
 config.adjust_window_size_when_changing_font_size = true
-config.font = wezterm.font_with_fallback{
+-- 中文显示
+config.font = wezterm.font_with_fallback {
   {
-    family = 'MonacoLigaturized Nerd Font Mono'
+    family = 'Maple Mono NF'
   },
   {
     family = 'LXGW WenKai Mono'
@@ -23,7 +37,7 @@ config.font = wezterm.font_with_fallback{
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.95
-config.window_decorations="RESIZE"
+config.window_decorations = "RESIZE"
 
 config.set_environment_variables = {
   TERMINFO_DIRS = '/home/user/.nix-profile/share/terminfo',
@@ -40,16 +54,16 @@ local function get_screen_size()
 end
 
 wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  local screen_width,screen_height = get_screen_size()
+  local _, _, window = mux.spawn_window(cmd or {})
+  local screen_width, screen_height = get_screen_size()
   wezterm.log_info(screen_width)
   wezterm.log_info(screen_height)
   local window_width = math.floor(screen_width * 0.85)
   local window_height = math.floor(screen_height * 0.80)
-    -- 计算窗口位置（居中）
+  -- 计算窗口位置（居中）
   local window_x = math.floor((screen_width - window_width) / 2)
   local window_y = math.floor((screen_height - window_height) / 2)
-    -- 设置窗口大小和位置
+  -- 设置窗口大小和位置
   local gui_window = window:gui_window()
   gui_window:set_inner_size(window_width, window_height)
   gui_window:set_position(window_x, window_y)
